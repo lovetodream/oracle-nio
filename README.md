11c8de20e4cd513fabd5a162a0972873e198a938
Timo <38291523+lovetodream@users.noreply.github.com>
2023-01-25 16:06:06 +0100

    feat: add option to run example with custom environment values

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ A Oracle Driver/Client which is 100% written in Swift using SwiftNIO, communicat
 Communication with Oracle DBs has no public documentation, because of that [python-oracledb](https://github.com/oracle/python-oracledb), especially the thin driver, is used as the main reference. 
 
 > Contributions in any way are appreciated. 
+
+## Developing and Tinkering
+
+To try things out you can run the embedded executable. 
+As of now only username and password authentication are supported.
+
+The following environment values are available to be set:
+
+ - `ORA_IP_ADDRESS`: IP Address of the Oracle Database
+ - `ORA_PORT`: Port of the Oracle Database (default: 1521)
+ - `ORA_SERVICE_NAME`: Service Name of the Oracle Database (default: XEPDB1)
+ - `ORA_USERNAME`: Username (default: my_user)
+ - `ORA_PASSWORD`: Password (default: my_passwor)
--- Sources/OracleNIOExample/main.swift
+++ Sources/OracleNIOExample/main.swift
@@ -1,11 +1,29 @@
 import OracleNIO
 import Foundation
 
+func env(_ name: String) -> String? {
+    ProcessInfo.processInfo.environment[name]
+}
+
 var logger = Logger(label: "com.lovetodream.oraclenio")
 logger.logLevel = .trace
 let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+let ipAddress = env("ORA_IP_ADDRESS") ?? "192.168.1.22"
+let port = (env("ORA_PORT").map(Int.init(_:)) ?? 1521) ?? 1521
+let serviceName = env("ORA_SERVICE_NAME") ?? "XEPDB1"
+let username = env("ORA_USERNAME") ?? "my_user"
+let password = env("ORA_PASSWORD") ?? "my_passwor"
 do {
-    _ = try OracleConnection.connect(using: .init(address: .init(ipAddress: "192.168.1.22", port: 1521), serviceName: "XEPDB1", username: "my_user", password: "my_passwor"), logger: logger, on: group.next()).wait()
+    _ = try OracleConnection.connect(
+        using: .init(
+            address: .init(ipAddress: ipAddress, port: port),
+            serviceName: serviceName,
+            username: username,
+            password: password
+        ),
+        logger: logger,
+        on: group.next()
+    ).wait()
 } catch {
     print(error)
 }

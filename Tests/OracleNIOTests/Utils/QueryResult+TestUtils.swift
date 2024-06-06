27ee8f7dcaac1173c58b8492c4fe49412ba6ed70
Timo <38291523+lovetodream@users.noreply.github.com>
2024-06-06 18:19:45 +0200

    chore: soundness

--- Tests/OracleNIOTests/Utils/QueryResult+TestUtils.swift
+++ Tests/OracleNIOTests/Utils/QueryResult+TestUtils.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the OracleNIO open source project
+//
+// Copyright (c) 2024 Timo Zacherl and the OracleNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import NIOCore
 import NIOEmbedded
 

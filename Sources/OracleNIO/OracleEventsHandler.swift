09d1025439e3268c29b1ae6f214950e177d5beb8
Timo <38291523+lovetodream@users.noreply.github.com>
2024-06-25 18:08:16 +0200

    refactor: make this change in a seperate pr as it requires a new test setup which would be out of scope

--- Sources/OracleNIO/OracleEventsHandler.swift
+++ Sources/OracleNIO/OracleEventsHandler.swift
@@ -80,7 +80,7 @@ final class OracleEventsHandler: ChannelInboundHandler {
                 break
             }
         default:
-            context.fireUserInboundEventTriggered(event)
+            preconditionFailure()
         }
     }
 

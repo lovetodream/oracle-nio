9d07fa7f79b6e50cfc0dda9bf51aa55d1920665c
Timo <38291523+lovetodream@users.noreply.github.com>
2024-10-11 09:33:47 +0200

    add support for bind lists (e.g. for WHERE IN clauses)

--- Tests/OracleNIOTests/OracleStatementTests.swift
+++ Tests/OracleNIOTests/OracleStatementTests.swift
@@ -117,6 +117,16 @@ final class OracleStatementTests: XCTestCase {
         XCTAssertEqual(query5.binds.bytes.readableBytes, 0)
         XCTAssertGreaterThan(query5.binds.longBytes.readableBytes, 0)
     }
+
+    func testBindList() {
+        let statement1: OracleStatement = "SELECT id FROM table WHERE id IN (\(list: [Int]()))"
+        XCTAssertEqual(statement1.sql, "SELECT id FROM table WHERE id IN ()")
+        XCTAssertEqual(statement1.binds.bytes.readableBytes, 0)
+
+        let statement2: OracleStatement = "SELECT id FROM table WHERE id IN (\(list: [1, 2, 3, 4, 5]))"
+        XCTAssertEqual(statement2.sql, "SELECT id FROM table WHERE id IN (:0, :1, :2, :3, :4)")
+        XCTAssertGreaterThan(statement2.binds.bytes.readableBytes, 0)
+    }
 }
 
 // Testing utility, because we do not have a throwing encodable, luckily :)

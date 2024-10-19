d33f3d55456f07a0798255545525206eefc82b96
Timo <38291523+lovetodream@users.noreply.github.com>
2024-10-19 11:00:11 +0100

    Add withTransaction (COMMIT on success, ROLLBACK on failure) to connection pool (#68)

--- Tests/IntegrationTests/OracleClientTests.swift
+++ Tests/IntegrationTests/OracleClientTests.swift
@@ -94,6 +94,62 @@ final class OracleClientTests: XCTestCase {
         }
     }
 
+    func testTransactionSuccess() async throws {
+        let config = try OracleConnection.testConfig()
+        let client = OracleClient(configuration: config, drcp: false, backgroundLogger: .oracleTest)
+        let runTask = Task {
+            await client.run()
+        }
+        try await client.withTransaction { connection in
+            _ = try? await connection.execute("DROP TABLE test_pool_transaction_success")
+            try await connection.execute("CREATE TABLE test_pool_transaction_success (id NUMBER)")
+            let affectedRows = try await connection.executeBatch(
+                "INSERT INTO test_pool_transaction_success (id) VALUES (:1)",
+                binds: Array(1...10)
+            ).affectedRows
+            XCTAssertEqual(affectedRows, 10)
+        }
+        try await client.withConnection { connection in
+            let stream = try await connection.execute("SELECT id FROM test_pool_transaction_success")
+            var index = 0
+            for try await id in stream.decode(Int.self) {
+                index += 1
+                XCTAssertEqual(index, id)
+            }
+            XCTAssertEqual(index, 10)
+        }
+        runTask.cancel()
+    }
+
+    func testTransactionFailure() async throws {
+        let config = try OracleConnection.testConfig()
+        let client = OracleClient(configuration: config, drcp: false, backgroundLogger: .oracleTest)
+        let runTask = Task {
+            await client.run()
+        }
+        do {
+            try await client.withTransaction { connection in
+                _ = try? await connection.execute("DROP TABLE test_pool_transaction_failure")
+                try await connection.execute("CREATE TABLE test_pool_transaction_failure (id VARCHAR2(1 byte))")
+                try await connection.executeBatch(
+                    "INSERT INTO test_pool_transaction_failure (id) VALUES (:1)",
+                    binds: Array(1...10).map({ "\($0)" })
+                )
+            }
+        } catch let error as OracleSQLError {
+            XCTAssertEqual(error.serverInfo?.affectedRows, 9)
+        }
+        try await client.withConnection { connection in
+            let rows =
+                try await connection
+                .execute("SELECT id FROM test_pool_transaction_failure")
+                .collect()
+                .count
+            XCTAssertEqual(rows, 0)
+        }
+        runTask.cancel()
+    }
+
     @available(macOS 14.0, *)
     func testPingPong() async throws {
         let idleTimeout = Duration.seconds(20)

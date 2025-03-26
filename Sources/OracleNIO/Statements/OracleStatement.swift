a38c0839f3a1aad8fb99b45aa8cace0cd5a820bf
Timo <38291523+lovetodream@users.noreply.github.com>
2025-03-26 11:22:12 +0100

    Add docc to initializers on OracleStatement (#80)

--- Sources/OracleNIO/Statements/OracleStatement.swift
+++ Sources/OracleNIO/Statements/OracleStatement.swift
@@ -23,6 +23,20 @@ public struct OracleStatement: Sendable, Hashable {
     /// The statement's binds.
     public var binds: OracleBindings
 
+    /// Creates an OracleStatement from a static SQL string and it's corresponding binds.
+    /// - Parameters:
+    ///   - sql: A static SQL string.
+    ///   - binds: A collection of binds.
+    ///
+    /// The amount of binds must match the number of variables in the SQL string.
+    ///
+    /// ```swift
+    /// let sql = "INSERT INTO my_table (content, initial_content, user_id) VALUES (:0, :0, :1)"
+    /// var binds = OracleBindings(capacity: 2) // 2 due to :0 and :1 being the variables in the statement.
+    /// binds.append("hello there")
+    /// binds.append(1)
+    /// let statement = OracleStatement(unsafeSQL: sql, binds: binds)
+    /// ```
     public init(
         unsafeSQL sql: String,
         binds: OracleBindings = OracleBindings()
@@ -38,7 +52,51 @@ extension OracleStatement: ExpressibleByStringInterpolation {
         self.binds = stringInterpolation.binds
     }
 
-    public init(stringLiteral value: StringLiteralType) {
+    /// Creates a SQL statement from a _raw string_ without binds.
+    /// - Parameter value: An unescaped String without binds.
+    ///
+    /// This initializer should only be used with static SQL statements that do not include variables.
+    ///
+    /// Here is an example of using the initializer, that might lead to SQL injection.
+    ///
+    /// ```swift
+    /// let input = "user_input"
+    /// let unsafeStatement = "INSERT INTO my_table (content) VALUES ('\(input)')"
+    /// print(OracleStatement(stringLiteral: unsafeStatement))
+    /// // -> INSERT INTO my_table (content) VALUES ('user_input') [ ]
+    /// // Potential SQL injection on unsanitized input!
+    /// ```
+    ///
+    /// - Warning: Use this initializer with extreme caution. String interpolations won't be recognized as such, and therefor binds are not created for them.
+    ///            Prefer using ``StringInterpolation`` or ``init(unsafeSQL:binds:)`` whenever possible.
+    ///
+    /// There are multiple ways to prevent SQL injection.
+    ///
+    /// ## StringInterpolation
+    ///
+    /// To create a ``OracleStatement`` with ``StringInterpolation``, all you need to do is add a type annotation to the statement.
+    /// This is the "magic" of string interpolation.
+    ///
+    /// ```swift
+    /// let input = "user_input"
+    /// let safeStatement: OracleStatement = "INSERT INTO my_table (content) VALUES (\(input))"
+    /// print(safeStatement)
+    /// // -> INSERT INTO my_table (content) VALUES (:0) [ **** ]
+    /// // The variable did now get correctly recognized as a bind.
+    /// ```
+    ///
+    /// ## Bind declaration
+    ///
+    /// Creating the binds manually is also possible, albeit more inconvinient.
+    ///
+    /// ```swift
+    /// let rawStatement = "INSERT INTO my_table (content) VALUES (:0)"
+    /// var binds = OracleBindings(capacity: 1)
+    /// binds.append(input)
+    /// print(OracleStatement(unsafeSQL: rawStatement, binds: binds))
+    /// // -> INSERT INTO my_table (content) VALUES (:0) [ **** ]
+    /// ```
+    public init(stringLiteral value: String) {
         self.sql = value
         self.binds = OracleBindings()
     }
@@ -146,6 +204,22 @@ extension OracleStatement {
             self.sql.removeLast(2)
         }
 
+        /// Adds an unescaped string to the statement.
+        /// - Parameter interpolation: The string that should be added to the statement.
+        ///
+        /// Useful when dynamically building a statement.
+        ///
+        /// ```swift
+        /// let ascending: Bool = true
+        /// let orderClause = if ascending {
+        ///     "ASC"
+        /// } else {
+        ///     "DESC"
+        /// }
+        /// let statement: OracleStatement = "SELECT name FROM table ORDER BY id \(unescaped: orderClause)"
+        /// print(statement)
+        /// // -> SELECT name FROM table ORDER BY id ASC [ ]
+        /// ```
         @inlinable
         public mutating func appendInterpolation(unescaped interpolation: String) {
             self.sql.append(contentsOf: interpolation)

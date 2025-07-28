6dbac824a0c35f3dfd5321220482f337dc347c58
Timo <38291523+lovetodream@users.noreply.github.com>
2025-07-28 13:41:21 +0200

    move min swift version to 6.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // Copyright 2024 Timo Zacherl
 // SPDX-License-Identifier: Apache-2.0
 
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.6.0"),
         .package(
             url: "https://github.com/swiftlang/swift-syntax.git",
-            "600.0.0-latest"..."601.0.1-latest"),
+            from: "601.0.0-latest"),
     ],
     targets: [
         .target(

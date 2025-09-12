669b884658b78f6492a03d77f4549bba5500601f
Timo <38291523+lovetodream@users.noreply.github.com>
2025-09-12 09:53:49 +0200

    fix soundness

--- Sources/OracleNIO/Connection/OracleTransactionConnection.swift
+++ Sources/OracleNIO/Connection/OracleTransactionConnection.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the OracleNIO open source project
 //
-// Copyright (c) 2024 Timo Zacherl and the OracleNIO project authors
+// Copyright (c) 2025 Timo Zacherl and the OracleNIO project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE for license information

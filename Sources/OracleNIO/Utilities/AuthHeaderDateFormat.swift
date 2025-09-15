c4a662c613e9fa0cf3599853328b30f17d8bea9f
Timo <38291523+lovetodream@users.noreply.github.com>
2025-09-15 17:54:51 +0200

    soundness

--- Sources/OracleNIO/Utilities/AuthHeaderDateFormat.swift
+++ Sources/OracleNIO/Utilities/AuthHeaderDateFormat.swift
@@ -13,9 +13,9 @@
 //===----------------------------------------------------------------------===//
 
 #if canImport(FoundationEssentials)
-import FoundationEssentials
+    import FoundationEssentials
 #else
-import Foundation
+    import Foundation
 #endif
 
 func authHeaderDate(for date: Date) -> String {
@@ -25,53 +25,55 @@ func authHeaderDate(for date: Date) -> String {
     let components = calendar.dateComponents([.weekday, .day, .month, .year, .hour, .minute, .second], from: date)
 
 
-    let weekday = switch components.weekday {
-    case 1:
-        "Sun"
-    case 2:
-        "Mon"
-    case 3:
-        "Tue"
-    case 4:
-        "Wed"
-    case 5:
-        "Thu"
-    case 6:
-        "Fri"
-    case 7:
-        "Sat"
-    default:
-        fatalError("Invalid weekday \(String(reflecting: components.weekday))")
-    }
+    let weekday =
+        switch components.weekday {
+        case 1:
+            "Sun"
+        case 2:
+            "Mon"
+        case 3:
+            "Tue"
+        case 4:
+            "Wed"
+        case 5:
+            "Thu"
+        case 6:
+            "Fri"
+        case 7:
+            "Sat"
+        default:
+            fatalError("Invalid weekday \(String(reflecting: components.weekday))")
+        }
     let day = String(components.day.unsafelyUnwrapped, padding: 2)
-    let month = switch components.month {
-    case 1:
-        "Jan"
-    case 2:
-        "Feb"
-    case 3:
-        "Mar"
-    case 4:
-        "Apr"
-    case 5:
-        "May"
-    case 6:
-        "Jun"
-    case 7:
-        "Jul"
-    case 8:
-        "Aug"
-    case 9:
-        "Sep"
-    case 10:
-        "Oct"
-    case 11:
-        "Nov"
-    case 12:
-        "Dec"
-    default:
-        fatalError("Invalid month \(String(reflecting: components.month))")
-    }
+    let month =
+        switch components.month {
+        case 1:
+            "Jan"
+        case 2:
+            "Feb"
+        case 3:
+            "Mar"
+        case 4:
+            "Apr"
+        case 5:
+            "May"
+        case 6:
+            "Jun"
+        case 7:
+            "Jul"
+        case 8:
+            "Aug"
+        case 9:
+            "Sep"
+        case 10:
+            "Oct"
+        case 11:
+            "Nov"
+        case 12:
+            "Dec"
+        default:
+            fatalError("Invalid month \(String(reflecting: components.month))")
+        }
     let year = String(components.year.unsafelyUnwrapped, padding: 4)
     let hour = String(components.hour.unsafelyUnwrapped, padding: 2)
     let minute = String(components.minute.unsafelyUnwrapped, padding: 2)

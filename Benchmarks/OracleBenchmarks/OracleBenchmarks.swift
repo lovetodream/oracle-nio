600c5a07622b82fc12014b84df188e6004072d78
Timo <38291523+lovetodream@users.noreply.github.com>
2026-09-23 21:13:40 +0200

    fix soundness
    Signed-off-by: Timo <38291523+lovetodream@users.noreply.github.com>

--- Benchmarks/OracleBenchmarks/OracleBenchmarks.swift
+++ Benchmarks/OracleBenchmarks/OracleBenchmarks.swift
@@ -51,7 +51,7 @@ extension Benchmark {
             server = Task {
                 try await OracleMockServer.run(port: port)
             }
-            await Task.sleep(nanoseconds: 100_000) // FIXME: hook up to server ready state instead
+            try await Task.sleep(nanoseconds: 100_000)  // FIXME: hook up to server ready state instead
             connection = try await OracleConnection.connect(
                 configuration: config,
                 id: 1
@@ -109,7 +109,7 @@ let benchmarks: @Sendable () -> Void = {
         server = Task {
             try await OracleMockServer.run(port: port)
         }
-        await Task.sleep(nanoseconds: 100_000) // FIXME: hook up to server ready state instead
+        try await Task.sleep(nanoseconds: 100_000)  // FIXME: hook up to server ready state instead
     } teardown: {
         server.cancel()
     }
